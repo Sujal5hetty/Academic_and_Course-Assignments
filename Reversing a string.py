bab9894fc9cf275333e08c5db373efed4b491efc
Sujal Shetty <120392287+Sujal5hetty@users.noreply.github.com>
2024-09-24 01:44:36 +0530

    Add files via upload

---
+++ Reversing a string.py
@@ -0,0 +1,7 @@
+def rev_string(s):
+    return s[::-1]
+
+my_string=input("Enter a string: ")
+reversed_str=rev_string(my_string)
+print("Original String: ",my_string)
+print("Reversed String: ",reversed_str)
